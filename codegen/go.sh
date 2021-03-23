cd9569a166799980a923c03c45bc9e15f27e8119
Ralph Pichler <pichler.ralph@gmail.com>
2021-03-23 11:00:40 +0100

    codegen

---
+++ codegen/go.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+TARGET="go"
+VERSION="1_0_0"
+
+yarn compile
+
+SIMPLESWAP_ABI="$(cat artifacts/contracts/ERC20SimpleSwap.sol/ERC20SimpleSwap.json | jq .abi)"
+SIMPLESWAP_FACTORY_ABI="$(cat artifacts/contracts/SimpleSwapFactory.sol/SimpleSwapFactory.json | jq .abi)"
+SIMPLESWAP_FACTORY_BYTECODE="$(cat artifacts/contracts/SimpleSwapFactory.sol/SimpleSwapFactory.json | jq .bytecode)"
+
+mkdir -p "$TARGET"
+cat <<EOF > "$TARGET/swap_abi_$VERSION.go"
+// Copyright 2021 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package swapabi
+
+const ERC20SimpleSwapABI = \`$SIMPLESWAP_ABI\`
+
+const SimpleSwapFactoryABI = \`$SIMPLESWAP_FACTORY_ABI\`
+
+const SimpleSwapFactoryBin = $SIMPLESWAP_FACTORY_BYTECODE
+EOF
