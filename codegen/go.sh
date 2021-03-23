5b7e7b3fb8596555317b29dd316409bf34f04425
Ralph Pichler <pichler.ralph@gmail.com>
2021-03-23 12:52:30 +0100

    add rc20

--- codegen/go.sh
+++ codegen/go.sh
@@ -7,6 +7,7 @@ VERSION="$(echo $2 | tr . _)"
 SIMPLESWAP_ABI="$(cat artifacts/contracts/ERC20SimpleSwap.sol/ERC20SimpleSwap.json | jq .abi)"
 SIMPLESWAP_FACTORY_ABI="$(cat artifacts/contracts/SimpleSwapFactory.sol/SimpleSwapFactory.json | jq .abi)"
 SIMPLESWAP_FACTORY_BYTECODE="$(cat artifacts/contracts/SimpleSwapFactory.sol/SimpleSwapFactory.json | jq .bytecode)"
+ERC20_ABI="$(cat artifacts/@openzeppelin/contracts/token/ERC20/ERC20.sol/ERC20.json | jq .abi)"
 
 cat <<EOF > "$TARGET"
 // Copyright 2021 The Swarm Authors. All rights reserved.
@@ -18,6 +19,8 @@ package swapabi
 const ERC20SimpleSwapABI$VERSION = \`$SIMPLESWAP_ABI\`
 
 const SimpleSwapFactoryABI$VERSION = \`$SIMPLESWAP_FACTORY_ABI\`
+
+const ERC20ABI$VERSION = \`$ERC20_ABI\`
 
 const SimpleSwapFactoryBin$VERSION = $SIMPLESWAP_FACTORY_BYTECODE
 EOF
